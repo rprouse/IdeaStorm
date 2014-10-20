f56cbc2b50d0eeb18aa5c77e926eb3bd80593b56
Rob Prouse <rob@prouse.org>
2014-10-19 22:17:31 -0400

    Continuing to update nuget packages

---
+++ IdeaStorm/Models/Experiment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IdeaStorm.Models
+{
+    public class Experiment
+    {
+        public Experiment()
+        {
+            Date = DateTime.Now;
+        }
+
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int Sequence { get; set; }
+        public string Customer { get; set; }
+        public string Problem { get; set; }
+        public string Solution { get; set; }
+        public string RiskiestAssumptions { get; set; }
+        public string Method { get; set; }
+        public string SuccessCriteria { get; set; }
+        public string Result { get; set; }
+        public string Decision { get; set; }
+        public string Learning { get; set; }
+        public bool Completed { get; set; }
+
+        public int IdeaId { get; set; }
+    }
+}
---
+++ IdeaStorm/Models/Idea.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IdeaStorm.Models
+{
+    public class Idea
+    {
+        public Idea()
+        {
+            Experiments = new List<Experiment>();
+            Date = DateTime.Now;
+        }
+
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ValueProposition { get; set; }
+        public string TargetMarket { get; set; }
+        public string UniqueSellingPropositon { get; set; }
+        public string RevenueModel { get; set; }
+        public string Competitors { get; set; }
+        public string StartupCosts { get; set; }
+        public string BarriersToEntry { get; set; }
+        public string NameIdeas { get; set; }
+        public string NicheEvaluation { get; set; }
+        public string Pros { get; set; }
+        public string Cons { get; set; }
+        public string Feedback { get; set; }
+        public int Rating { get; set; }
+
+        public ICollection<Experiment> Experiments { get; set; } 
+    }
+}

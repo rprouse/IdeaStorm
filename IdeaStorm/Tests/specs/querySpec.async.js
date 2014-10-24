3b3fd75036d23d99898c7448800793204ab46cb7
Rob Prouse <rob@prouse.org>
2014-10-24 16:47:16 -0400

    Added web api unit tests

--- IdeaStorm/Tests/specs/querySpec.async.js
+++ IdeaStorm/Tests/specs/querySpec.async.js
@@ -15,7 +15,6 @@ describe('when querying the breeze api', function() {
   'use strict';
 
   var fns = window.testFns;
-  var query = breeze.EntityQuery;
   var em;
   var failed = fns.failed;
 
@@ -27,17 +26,62 @@ describe('when querying the breeze api', function() {
 
   });
 
-  it('should get at least one idea', function(done) {
-    query.from("Ideas")
-      .using(em)
-      .execute()
-      .then(success)
-      .catch(failed)
-      .finally(done);
+  it('should get at least one idea', function (done) {
+    testQuery("Ideas", success, done);
 
     function success(data) {
       var results = data.results;
       expect(results.length).toBeGreaterThan(0);
     }
   });
+
+  it('should get at least one experiment', function (done) {
+    testQuery("Experiments", success, done);
+
+    function success(data) {
+      var results = data.results;
+      expect(results.length).toBeGreaterThan(0);
+    }
+  });
+
+  it('should find an idea with its experiments', function(done) {
+    testQuery("Ideas", success, done, addToQuery);
+
+    function addToQuery(query) {
+      return query.top(5).expand('Experiments');
+    }
+
+    function success(data) {
+      var results = data.results;
+      expect(results.length).toBe(5);
+      var idea = results[4];
+      expect(idea.Experiments.length).toBeGreaterThan(0);
+    }
+  });
+
+  it('should find idea with id 5', function (done) {
+    testQuery("Ideas", success, done, addToQuery);
+
+    function addToQuery(query) {
+      return query.where('Id', 'eq', 5);
+    }
+
+    function success(data) {
+      var results = data.results;
+      expect(results.length).toBe(1);
+      var idea = results[0];
+      expect(idea.Id).toBe(5);
+    }
+  });
+
+  function testQuery(resourceName, success, done, addToQuery) {
+    var query = breeze.EntityQuery.from(resourceName).using(em);
+    if (addToQuery) {
+      query = addToQuery(query);
+    }
+    return query.execute()
+      .then(success)
+      .catch(failed)
+      .finally(done);
+  }
 });
